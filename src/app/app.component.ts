8cd3fc9c5b33a458e3341acf4206e99d019ffe08
Israa-Burhan <israaburhan97@gmail.com>
2025-10-06 15:16:42 +0300

    edit app.commpnent.ts just remove import navbar and footer commponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './modules/shared/components/navbar/navbar.component';
-import { FooterComponent } from './modules/shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './modules/shared/components/spinner/spinner.component';
 import { supabase } from './core/services/supabase.client';
@@ -9,7 +7,7 @@ import { supabase } from './core/services/supabase.client';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, NavbarComponent, FooterComponent, SpinnerComponent],
+  imports: [CommonModule, RouterOutlet, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
