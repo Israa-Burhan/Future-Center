79e36baa6d5f4d45b7eb28aeaab20c1e3ff4502f
Israa-Burhan <israaburhan97@gmail.com>
2025-10-07 20:21:23 +0300

    create not found page and add class container to navbar

--- src/app/modules/shared/components/not-found/not-found.component.ts
+++ src/app/modules/shared/components/not-found/not-found.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
